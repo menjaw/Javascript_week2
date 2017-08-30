6231cd479cdc00318286b4bbdb2b363a26454961
menjaw <menjaholm@gmail.com>
2017-08-30 22:21:45 +0200

    callback test method added

--- CallbackPractises/callbackTest1.js
+++ CallbackPractises/callbackTest1.js
@@ -8,3 +8,20 @@ friends.forEach(function (eachItem, index) {
     console.log(index + 1 + ":" + eachItem);
 });
 
+
+//Here we create a test callback function
+function useCallback(callback, callback2) {
+    console.log("This is the function that is used to run the callback function");
+    callback();
+    callback2();
+}
+
+function callbackFunction() {
+    console.log("This is the callback function");
+}
+
+function anotherCallback() {
+    console.log("This is another callback function");
+}
+
+useCallback(callbackFunction, anotherCallback);
