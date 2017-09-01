8ceaa62979db314b0e0f67b521a8c53bafffdd65
menjaw <menjaholm@gmail.com>
2017-09-01 11:48:19 +0200

    startet on exercise 4 but it sucks

---
+++ DomManipulationEvents/members.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Menja on 01-09-2017.
+ */
+var members = ["Toby", "Dash", "Hillary", "Cliff", "George", "Phil"];
+
+
+function convertArrayToString(members) {
+    members.join(" - ");
+    return members;
+}
+
+var result = convertArrayToString(members);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(typeof members);
+// console.log(typeof convertToString);
+// function convertToString(members) {
+//     return members.join();
+//
+// }
+// console.log(members.map(function (names) {
+//     return names;
+//
+// }))
