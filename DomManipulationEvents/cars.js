263d8c39cf6eae554c8aecadd6504149c15789d2
menjaw <menjaholm@gmail.com>
2017-09-05 12:14:06 +0200

    files to assignment with cars added

---
+++ DomManipulationEvents/cars.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Menja on 04-09-2017.
+ */
+//Declare array
+var cars = [
+    {id: 1, year: 1997, make: 'Ford', model: 'E350', price: 3000},
+    {id: 2, year: 1999, make: 'Chevy', model: 'Venture', price: 4900},
+    {id: 3, year: 2000, make: 'Jeep', model: 'V70', price: 5000},
+    {id: 4, year: 1996, make: 'Volvo', model: 'Grand Cherokee', price: 2500},
+    {id: 5, year: 2005, make: 'Chevy', model: 'Venture', price: 3890}
+];
+
